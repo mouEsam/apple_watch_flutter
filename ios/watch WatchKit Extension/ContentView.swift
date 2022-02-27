42285af6e34161cc5232ef647c4572d8d2744f0a
Moustafa Essam <drsh_201122@yahoo.com>
2022-02-27 13:51:58 +0200

    first commit

---
+++ ios/watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  watch WatchKit Extension
+//
+//  Created by Mostafa Ibrahim on 27/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var viewModel: WatchViewModel = WatchViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Counter: \(viewModel.counter)")
+                .padding()
+            Button(action: {
+                viewModel.sendDataMessage(for: .sendCounterToFlutter, data: ["counter": viewModel.counter + 1])
+            }) {
+                Text("+ by 2")
+            }
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/watch WatchKit Extension/RunnerApp.swift
@@ -0,0 +1,19 @@
+//
+//  RunnerApp.swift
+//  watch WatchKit Extension
+//
+//  Created by Mostafa Ibrahim on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
